07311b4efc20a47304d158071db0d96c8cda61e3
Administrator <849183239@qq.com>
2019-11-10 11:16:05 +0800

    update Configure

--- src/main/java/com/studycloud/client/ConfigApplication.java
+++ src/main/java/com/studycloud/client/ConfigApplication.java
@@ -1,15 +1,17 @@
-package com.imooc.client;
+package com.studycloud.client;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
 @EnableEurekaClient
-public class ClientApplication {
+@EnableConfigServer
+public class ConfigApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(ClientApplication.class, args);
+        SpringApplication.run(ConfigApplication.class, args);
     }
 
 }
